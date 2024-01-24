3a9db053d0990f98fa691322a20c8ef7054026c0
Daniel Diaz Martinez <diazmartinezdaniel30@gmail.com>
2024-01-24 12:46:16 +0100

    Creacion de entity book e impletancion ORM

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -11,38 +11,39 @@ import {
 import { BooksService } from './books.service';
 import { Request } from 'express';
 import { BookDto } from './book.dto';
-import { Book } from './book.class';
+import { Book } from './book.entity'; 
+
 @Controller('books')
 export class BooksController {
 
   constructor(private booksService: BooksService) {}
 
-    @Get()
-    findAll(@Req() request: Request): Book[] {
-      console.log(request.query);
-      return this.booksService.findAll(request.query);
-    }
+  @Get()
+  findAll(@Req() request: Request): Promise<Book[]> { 
+    console.log(request.query);
+    return this.booksService.findAll(request.query);
+  }
 
-    @Post()
-    createBook(@Body() newBook: BookDto) { 
-      return this.booksService.createBook(newBook); 
-    }
+  @Get(':bookId')
+  findBook(@Param('bookId') bookId: string): Promise<Book> {
+    return this.booksService.findBook(bookId);
+  }
 
-    @Get(':bookId') 
-    findBook(@Param('bookId') bookId: string): Book {
-      return this.booksService.findBook(bookId);
-    }
+  @Post()
+  createBook(@Body() newBook: BookDto): Promise<Book> { 
+    return this.booksService.createBook(newBook);
+  }
 
-    @Delete(':bookId') 
-    deleteBook(@Param('bookId') bookId: string): Book {
-      return this.booksService.deleteBook(bookId);
-    }
-  
+  @Delete(':bookId')
+  deleteBook(@Param('bookId') bookId: string): Promise<Book> {
+    return this.booksService.deleteBook(bookId);
+  }
 
-    @Put(':bookId')
-    updateBook(@Param('bookId') bookId: string, @Body() newBook: BookDto): Book {
-      return this.booksService.updateBook(bookId, newBook);
-    }
-
+  @Put(':bookId')
+  updateBook(
+    @Param('bookId') bookId: string,
+    @Body() newBook: BookDto, 
+  ): Promise<Book> {
+    return this.booksService.updateBook(bookId, newBook);
+  }
 }
-
